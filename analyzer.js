714ef5bf765619ce59961cc1db93973bfeab6ac8
James Nguyen <nguyenjaug98@gmail.com>
2017-09-19 18:25:46 -0400

    Add files via upload

---
+++ analyzer.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+  "username": "8215bd94-46f1-4875-8102-ede783bf7260",
+  "password": "SEyonnt56bzV",
+  version_date: "2016-05-19"
+}
---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'hVTWfkfb1Rz1uwNUea7A1TDMA',
+  consumer_secret:      'zjlSqjzslWC5tHmQzJ93M3QkmisY2CfyJfkNmyRLoF2889DQdx',
+  access_token:         '909251216324136960-jeowGq4T3LKjr2qwKlCzdWrDXSCCs5p',
+  access_token_secret:  '0ADVGJAuT7VDwV5gVzj2ZKFTpzzEFqTmErXImphLVdY0Y',
+  timeout_ms:           60*1000,
+}
